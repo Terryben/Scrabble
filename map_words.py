330219f6ebdc5d7378897d27a0943e334aa04879
brterry <Ben18181820@gmail.com>
2014-12-06 13:15:14 -0700

    added position finding

--- map_words.py
+++ map_words.py
@@ -1,5 +1,11 @@
 #! /usr/bin/python
 import sys
+class map_words():
+
+def __init__ (self):
+  self.dictionary = readWords(words.txt)
+  self.alphScore = alphabetScore()
+
 def readWords(text_file):
   f = open(text_file)
   dictionary = {}
@@ -12,11 +18,40 @@ def alphabetScore():
   alphScore = {'e':1, 'a':1,'i':1,'o':1,'n':1,'r':1,'t':1,'l':1,'s':1,'u':1,'d':2,'g':2,'b':3,'c':3,'m':3,'p':3,'f':4,'h':4,'v':4,'w':4,'y':4,'k':5,'j':8,'x':8,'q':10,'z':10}
   return alphScore
 
-def wordScore(word, alphScore):
+def findWordPos(tup1, tup2):
+  posArray = []
+  if ((tup1[0] - tup2[0]) == 0):  #the word is along the y axis
+    for i in range(0, tup2[1]-tup1[1]):
+      letterlocal = (tup1[0], tup1[1]+i)
+      posArray.append(letterlocal)
+    return posArray
+  else:        #the word is along the x axis
+    for i in range(0, tup2[0]-tup1[0]):
+      letterlocal = (tup1[0]+i, tup1[1])
+      posArray.append(letterlocal)
+    return posArray
+
+def wordScore(word, alphScore, tup1, tup2):
+  wordPos = findWordPos(tup1, tup2)
+  print "The wordPos is: "
+  print wordPos
   word = word.lower()
   score = 0
+  word_mult = 1
+  letter_mult = 1
   for i in range(0,len(word)):
-    score += alphScore[word[i]]
+    if wordPos[i] == 'TW':
+      word_mult = word_mult*3
+    elif wordPos[i] == 'DW':
+      word_mult = word_mult*2
+    elif wordPos[i] = 'TL':
+      letter_mult = 3
+    elif wordPos[i] = 'DL':
+      letter_mult = 2
+    else:
+    score += alphScore[word[i]]*letter_mult
+    letter_mult = 1
+  score = score*word_mult
   return score
 
 def isValidWord(word):
@@ -30,17 +65,17 @@ def isValidWord(word):
     return False
   
 
-if __name__== "__main__":
-  if len(sys.argv) < 2:
-    print "Please provide a text file to create the dictionary."
-    sys.exit()
+#if __name__== "__main__":
+#  if len(sys.argv) < 2:
+#    print "Please provide a text file to create the dictionary."
+#    sys.exit()
   
-  dictionary = readWords(sys.argv[1])
-  num_args = len(sys.argv)
-  alphScore = alphabetScore()
+#  dictionary = readWords(sys.argv[1])
+#  num_args = len(sys.argv)
+#  alphScore = alphabetScore()
   #print dictionary.keys()
-  while num_args > 2:
-    word = sys.argv[num_args-1]
-    print word + " is a valid word? " + str(isValidWord(word))
-    print "The word score is: " + str(wordScore(word, alphScore))
-    num_args = num_args - 1
+#  while num_args > 2:
+#    word = sys.argv[num_args-1]
+#    print word + " is a valid word? " + str(isValidWord(word))
+#    print "The word score is: " + str(wordScore(word, alphScore, (1,2), (1, 5)))
+#    num_args = num_args - 1
